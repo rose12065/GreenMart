29ba473af02aa1d261c55ddf49a3233960fe312c
rose12065 <77180302+rose12065@users.noreply.github.com>
2023-09-17 20:31:01 +0530

    Update google_config.php

--- google_config.php
+++ google_config.php
@@ -12,10 +12,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('527263714944-ban5ot2sdpp5nufjui2gvfgf9bdnpj1d.apps.googleusercontent.com');
+$google_client->setClientId('');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('http://localhost/GreenMart/login.php');
